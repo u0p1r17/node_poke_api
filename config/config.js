b51b6936b9771e24e31bbb6f6a6f94d324db1b0d
Liridon Cerimi <liridoncerimi@MBP-de-Liridon.home>
2023-01-31 02:59:47 +0100

    edit position of key from 3 to 1 position and edit port of app to MYSQLPORT

--- config/config.js
+++ config/config.js
@@ -13,9 +13,9 @@ module.exports = {
     }
   },
   production: { 
+    "database": process.env.MYSQLDATABASE,
     "username": process.env.MYSQLUSER,
     "password": process.env.MYSQLPASSWORD,
-    "database": process.env.MYSQLDATABASE,
     "host": process.env.MYSQLHOST,
     "port": process.env.MYSQLPORT,  
     "dialect": 'mysql'
